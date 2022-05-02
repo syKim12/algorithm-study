87014270b6ff0a08738559efa7fddb53f67e5c6d
syKim12 <fromsykim@gmail.com>
2022-05-03 00:16:21 +0900

    upload keypad problem on programmers

--- programmers/keypad.py
+++ programmers/keypad.py
@@ -1,3 +1,52 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
 def solution(numbers, hand):
     answer = ''
     nl = (3, 0)
@@ -18,21 +67,31 @@ def solution(numbers, hand):
             elif k == 6:
                 nr = (1, 2)
             else:
-                nr = (2, 3)
-        else:
+                nr = (2, 2)
+        elif k in [2,5,8,0]:
             if k == 2:
                 current = (0,1)
             elif k == 5:
                 current = (1,1)
             elif k == 8:
                 current = (2, 1)
-            else:
+            elif k == 0:
                 current = (3,1)
             ldist = abs(nl[0]-current[0]) + abs(nl[1]-current[1])
             rdist = abs(nr[0]-current[0]) + abs(nr[1]-current[1])
             if ldist < rdist:
                 answer += 'L'
-            
-                    
-            
+                nl = current
+            elif ldist > rdist:
+                answer += 'R'
+                nr = current
+            else:
+                if hand == 'left':
+                    answer += 'L'
+                    nl = current
+                else:
+                    answer += 'R'
+                    nr = current
+
+
     return answer
