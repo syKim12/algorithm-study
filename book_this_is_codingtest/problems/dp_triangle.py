02d0aead55538bc30f6e8a93ed6f71413102a3d4
syKim12 <fromsykim@gmail.com>
2022-05-29 21:39:56 +0900

    upload dp problems

---
+++ book_this_is_codingtest/problems/dp_triangle.py
@@ -0,0 +1,21 @@
+n = int(input())
+dp = []
+for i in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1,n):
+    for j in range(i):
+        #왼쪽 위에서 오는 경우
+        if j==0:
+            up_left = 0
+        else:
+            up_left = dp[i-1][j-1]
+        #바로 위에서 오는 경우
+        if j == i:
+            up = 0
+        else:
+            up = dp[i-1][j]
+
+        dp[i][j] = dp[i][j] + max(up, up_left)
+
+print(max(dp[n-1]))
