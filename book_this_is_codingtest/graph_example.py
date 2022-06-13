956d53eea05d044e99f25ec52a1162b5b87025e9
syKim12 <fromsykim@gmail.com>
2022-06-13 21:22:27 +0900

    upload graph example code

---
+++ book_this_is_codingtest/graph_example.py
@@ -0,0 +1,6 @@
+#특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    #루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
