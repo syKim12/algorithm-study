2cc7562083c76efa053f54aee5c5e33bb94e6ca0
syKim12 <fromsykim@gmail.com>
2022-06-26 23:58:26 +0900

    spanning tree

---
+++ book_this_is_codingtest/spanning_tree.py
@@ -0,0 +1,22 @@
+#특정 원소가 속한 집합 찾기
+def find_parent(parent, x):
+    #루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+#두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+#노드 개수와 간선 개수 입력 받기
+v, e = map(int, input().split())
+parent = [0]*(v+1)
+
+edges= []
+result = 0
