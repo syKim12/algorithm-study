d4f3ffa8a71a7569d91b9ad53e4381158b1139f2
syKim12 <fromsykim@gmail.com>
2022-07-06 23:57:54 +0900

    upload programmers problem

---
+++ programmers/joystick.py
@@ -0,0 +1,15 @@
+def solution(name):
+    answer = 0
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    length = len(name)
+    #초기값 설정
+    result = "A"*length
+    answer += alphabet.find(name[0])
+    result[0] = name[0]
+    
+    for i in range(1,length):
+        if name[i] == "A":
+            continue
+        
+        
+    return answer
