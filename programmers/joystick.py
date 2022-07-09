6c34d05bc95a23449823321b550c6cd127184ebb
syKim12 <fromsykim@gmail.com>
2022-07-09 23:59:03 +0900

    upload joystick problem

--- programmers/joystick.py
+++ programmers/joystick.py
@@ -7,14 +7,19 @@ def solution(name):
     length = len(name)
     #초기값 설정
     result = "A"*length
-    answer += alphabet.find(name[0])
-    result[0] = name[0]
+    #answer += alphabet.find(name[0])
+    #result[0] = name[0]
     
-    for i in range(1,length):
+    for i in range(length):
+        if i == 0 and name[1:-1] == "A"*(length-2):
+            answer += 1
+            
         if result == name:
             break
         if name[i] == "A":
             continue
+        answer += alphabet.find(name[i])
+        result[i] = name[i]
         
         
     return answer
