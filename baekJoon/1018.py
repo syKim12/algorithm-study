42a12b9e179d112aef01ce5dc70d446ae7a3b924
fromsykim <fromsykim@gmail.com>
2023-01-31 23:13:17 +0900

    brute forth

--- baekJoon/1018.py
+++ baekJoon/1018.py
@@ -1,5 +1,30 @@
 import sys
 
+def make_board(color):
+    if color == "W":
+        check_board = [
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+        ]
+    else:
+        check_board = [
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+        ]
+    return check_board
+
 N, M = map(int, sys.stdin.readline().split())
 arr = [0]*N 
 visited = [[0]*M for _ in range(N)]
@@ -7,37 +32,16 @@ candidates = []
 for i in range(N):
     arr[i] = sys.stdin.readline().strip()
 min_cnt = 1e9
-"""
-def sol(path, visited, idx):
-    if idx == N:
-        cnt = 0
-        one_array = ''
 
-    for i in range(0, N-6): # 세로 i
-        for j in range(7, M-6): # 가로 j
-            if not visited[i][j]:
-                visited[i][j] = 1
-                path.append((i, j))
-                sol(path, visited, idx + 1)
-                visited[i][j] = 0
-                path.pop()
-"""
-for i in range(7, N-6):
-    for j in range(0, M-6):
+for i in range(0, N-7): #세로
+    for j in range(0, M-7):#가로
         cnt = 0
-        one_array = ''
-        for x in range(
+        check = make_board(arr[i][j])
+        for m in range(i, i+8): 
+            for n in range(8): 
+                if arr[m][j+n] != check[m%8][n]:
+                    cnt += 1   
+        min_cnt = min(cnt, min_cnt)
+print(min_cnt)    
         
-
-
-"""
-8 8
-WBWBWBWB
-BWBWBWBW
-WBWBWBWB
-BWBBBWBW
-WBWBWBWB
-BWBWBWBW
-WBWBWBWB
-BWBWBWBW
-""" 
+# 10 10 케이스
