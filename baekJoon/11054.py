8ce98829334e98e46b1dae7ae5eb97572e7a6573
syKim12 <fromsykim@gmail.com>
2023-02-13 23:49:49 +0900

    sooyeon: dp

--- baekJoon/11054.py
+++ baekJoon/11054.py
@@ -6,6 +6,25 @@ reverse_nlist = nlist[::-1]
 increasing = [1]*N
 decreasing = [1]*N
 
+# 348ms
+for i in range(N):
+    for j in range(i):
+        if nlist[i] > nlist[j]:
+            increasing[i] = max(increasing[i], increasing[j]+1)
+        if nlist[N-i-1] > nlist[N-j-1]:
+            decreasing[N-i-1] = max(decreasing[N-i-1], decreasing[N-j-1]+1)
+# 268ms
+"""
+for i in range(N):
+    for j in range(i):
+        if nlist[i] > nlist[j]:
+            increasing[i] = max(increasing[i], increasing[j]+1)
+for i in range(N-1, -1, -1):
+    for j in range(N-1, i, -1):
+        if nlist[i] > nlist[j]:
+            decreasing[i] = max(decreasing[i], decreasing[j]+1) 
+"""       
+"""
 for i in range(N):
     for j in range(i):
         if nlist[i] > nlist[j]:
@@ -13,27 +32,11 @@ for i in range(N):
         if reverse_nlist[i] > reverse_nlist[j]:
             decreasing[i] = max(decreasing[i], decreasing[j]+1)
 """
-for i in range(N):
-    temp_max = -1
-    index = -1
-    for j in range(i):
-        if nlist[i] > nlist[j] and nlist[j] >= temp_max:
-            temp_max = nlist[j]
-            index = j
-    if index >= 0:
-        increasing[i] = increasing[index] + 1
 
-for i in range(N-1, -1, -1):
-    temp_max = 0
-    index = -1
-    for j in range(N-1, i, -1):
-        if nlist[i] > nlist[j] and nlist[j] >= temp_max:
-            temp_max = nlist[j]
-            index = j
-    if index >= 0:
-        decreasing[i] = decreasing[index] + 1
-"""
 #print(increasing)
 #print(decreasing)
-lis = [increasing[i]+decreasing[N-i-1]-1 for i in range(N)]
-print(max(lis))
+#lis = [increasing[i]+decreasing[N-i-1]-1 for i in range(N)]
+lis = [increasing[i]+decreasing[i]-1 for i in range(N)]
+print(increasing)
+print(decreasing)
+print(lis)
--- baekJoon/2133.py
+++ baekJoon/2133.py
@@ -1,6 +1,36 @@
 N = int(input())
 if N % 2 == 1: # 홀수
     print(0)
+    exit(0)
+elif N == 2:
+    print(3)
+    exit(0)
+elif N == 4:
+    print(11)
+    exit(0)
+div = N // 2
+dp = [0]*(div+1)
+
+dp[1], dp[2] = 3, 11
+
+
+
+
+"""
+for k in range(3, div+1):
+    flag_unique = True
+    for i in range(1, k):
+        for j in range(k-1, 0, -1):
+            if i + j == k:
+                if flag_unique:
+                    flag_unique = False
+                    dp[k] += dp[i]*dp[j]
+                else:
+                    dp[k] += dp[j]*2
+    dp[k] += 2
+print(dp[-1])
+"""
+"""    
 elif N == 2:
     print(3)
 elif N == 4:
@@ -19,3 +49,4 @@ else:
                     dp[div] = dp[i]*dp[j]*2+2
     #print(dp)
     print(dp[div])
+"""
