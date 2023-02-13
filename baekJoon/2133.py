40ef0aa38c3b39b9f4ac61b2ad23d49113d874b8
syKim12 <fromsykim@gmail.com>
2023-02-14 01:37:50 +0900

    sooyeon: 230214 dp

--- baekJoon/2133.py
+++ baekJoon/2133.py
@@ -14,39 +14,15 @@ dp = [0]*(div+1)
 dp[1], dp[2] = 3, 11
 
 
-
-
-"""
 for k in range(3, div+1):
     flag_unique = True
-    for i in range(1, k):
-        for j in range(k-1, 0, -1):
-            if i + j == k:
-                if flag_unique:
-                    flag_unique = False
-                    dp[k] += dp[i]*dp[j]
-                else:
-                    dp[k] += dp[j]*2
+    for i in range(1, div):
+        if flag_unique:
+            flag_unique = False
+            dp[k] += dp[i]*dp[k-i]
+        else:
+            dp[k] += dp[k-i]*2
     dp[k] += 2
+#print(dp)
 print(dp[-1])
-"""
-"""    
-elif N == 2:
-    print(3)
-elif N == 4:
-    print(11)
-else:
-    div = N // 2 
-    dp = [0]*(div+1)
-    dp[1], dp[2] = 3, 11
-    flag = True
-    for i in range(1, div+1):
-        for j in range(div, 0, -1):
-            if i + j == div:
-                if flag:
-                    dp[div] += dp[i]*dp[j]
-                else:
-                    dp[div] = dp[i]*dp[j]*2+2
-    #print(dp)
-    print(dp[div])
-"""
+
